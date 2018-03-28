3deb8e27da306400ad724ae2e04fb5c357b3e1bf
cdclaxton <c.d.claxton@gmail.com>
2018-03-28 22:06:09 +0100

    Added time signature as an enum.

--- src/main/java/com/github/cdclaxton/music/Bar.java
+++ src/main/java/com/github/cdclaxton/music/Bar.java
@@ -8,10 +8,23 @@ public class Bar {
     private List<Note> notes;
     private List<TimedChord> timedChords;
 
+    public enum TimeSignature { Four4, Six8 }
+
     public Bar(TimeSignature timeSignature, List<Note> notes, List<TimedChord> timedChords) {
         this.timeSignature = timeSignature;
         this.notes = notes;
         this.timedChords = timedChords;
     }
 
+    public TimeSignature getTimeSignature() {
+        return timeSignature;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public List<TimedChord> getTimedChords() {
+        return timedChords;
+    }
 }
