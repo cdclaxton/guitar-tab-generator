e22b0e0f66771a9801093e9fac9f9cca0ac22c81
cdclaxton <c.d.claxton@gmail.com>
2018-03-30 11:53:14 +0100

    Started the tab parsing class.

---
+++ src/main/java/com/github/cdclaxton/tabparser/TabParser.java
@@ -0,0 +1,13 @@
+package com.github.cdclaxton.tabparser;
+
+import com.github.cdclaxton.music.Note;
+
+public class TabParser {
+
+    private TabParser() {}
+
+    protected Note parseSingleNote(String note) {
+        return null;
+    }
+
+}
