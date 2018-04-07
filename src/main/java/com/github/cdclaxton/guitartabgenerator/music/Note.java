ee90f5f2b06a40cc106b5bdb333aeff36b69d700
Chris <c.d.claxton@gmail.com>
2018-04-07 21:12:57 +0100

    Added equals() and hashCode() methods.

--- src/main/java/com/github/cdclaxton/guitartabgenerator/music/Note.java
+++ src/main/java/com/github/cdclaxton/guitartabgenerator/music/Note.java
@@ -1,5 +1,7 @@
 package com.github.cdclaxton.guitartabgenerator.music;
 
+import java.util.Objects;
+
 public class Note {
 
     private Fret fret;
@@ -16,5 +18,25 @@ public class Note {
 
     public Timing getTiming() {
         return timing;
+    }
+
+    @Override
+    public String toString() {
+        return "Note[" + this.fret.toString() + "," + this.timing.toString() + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return Objects.equals(fret, note.fret) &&
+                Objects.equals(timing, note.timing);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(fret, timing);
     }
 }
