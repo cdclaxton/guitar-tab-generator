b097653c9cf43d67817eb11f2db0cd256cd5c24f
cdclaxton <c.d.claxton@gmail.com>
2018-04-10 21:07:40 +0100

    Added code to launch a web browser.

---
+++ src/main/java/com/github/cdclaxton/guitartabgenerator/app/WebBrowserLauncher.java
@@ -0,0 +1,27 @@
+package com.github.cdclaxton.guitartabgenerator.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class WebBrowserLauncher {
+
+    private static Logger logger = LoggerFactory.getLogger(WebBrowserLauncher.class);
+
+    /**
+     * Launch a web browser to show the url. Windows only.
+     *
+     * @param url URL to open in web browser.
+     */
+    public static void launch(String url) {
+        Runtime rt = Runtime.getRuntime();
+        try {
+            rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
+        } catch (IOException e) {
+            logger.error("Couldn't launch browser for URL: " + url);
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/test/java/com/github/cdclaxton/guitartabgenerator/app/WebBrowserLauncherTest.java
@@ -0,0 +1,18 @@
+package com.github.cdclaxton.guitartabgenerator.app;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Ignore
+class WebBrowserLauncherTest {
+
+    @Test
+    void testLaunch() {
+        String url = "https://www.youtube.com/watch?v=vg5qDljEw7Q";
+        WebBrowserLauncher.launch(url);
+    }
+
+
+}
