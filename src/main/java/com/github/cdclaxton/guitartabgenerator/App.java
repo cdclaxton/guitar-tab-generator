4bda7356d277c219db7541775d84c3dd719760a5
Chris <c.d.claxton@gmail.com>
2018-04-10 09:52:58 +0100

    Modified POM file to create an Uber jar.

--- src/main/java/com/github/cdclaxton/guitartabgenerator/App.java
+++
@@ -1,9 +0,0 @@
-package com.github.cdclaxton.guitartabgenerator;
-
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Guitar music generator.");
-    }
-
-}
---
+++ src/main/java/com/github/cdclaxton/guitartabgenerator/app/GuitarTabGenerator.java
@@ -0,0 +1,20 @@
+package com.github.cdclaxton.guitartabgenerator.app;
+
+public final class GuitarTabGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+    public static void generateTab(String inputFile, String transposeKey, boolean transposeUp) {
+
+        // Read the input file (if it exists)
+
+
+        // Transpose the music
+
+        // Write the tab to a file
+
+    }
+
+}
