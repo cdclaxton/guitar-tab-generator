b39b646012d2788f76a29239829bb90d73828033
Chris <c.d.claxton@gmail.com>
2018-04-14 22:06:13 +0100

    Made member variables immutable and added hashCode() and equals() methods.

--- src/main/java/com/github/cdclaxton/guitartabgenerator/app/Config.java
+++ src/main/java/com/github/cdclaxton/guitartabgenerator/app/Config.java
@@ -3,18 +3,19 @@ package com.github.cdclaxton.guitartabgenerator.app;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 import java.util.Properties;
 
-public class Config {
+public final class Config {
 
-    private int pageWidth;
-    private int maxFret;
+    private final int pageWidth;
+    private final int maxFret;
 
     /**
      * Instantiate the config a config.properties file.
      *
      * @param path Path of the config file.
-     * @throws IOException
+     * @throws IOException Unable to read the config file.
      */
     Config(String path) throws IOException {
         Properties prop = new Properties();
@@ -45,5 +46,19 @@ public class Config {
     @Override
     public String toString() {
         return "Config[pageWidth=" + this.pageWidth + ",maxFret=" + this.maxFret + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Config config = (Config) o;
+        return pageWidth == config.pageWidth &&
+                maxFret == config.maxFret;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pageWidth, maxFret);
     }
 }
